8c015e12dca5989d034ebfcf6e9608f8d713aa4a
Odirile Masemola <odirilemasemola1@gmail.com>
2026-04-14 08:58:39 +0200

    Add EF Core DbContext, controllers, and Swagger support
    Introduce empty controllers for Course, Lecturer, and Student. Add DatabaseContext with DbSet properties for main entities to enable EF Core. Update Swagger/OpenAPI setup in Program.cs. Add necessary NuGet packages for EF Core and Swagger; update project folders.

---
+++ Controllers/LecturerController.cs
@@ -0,0 +1,6 @@
+﻿namespace Student_Management_API.Controllers
+{
+    public class LecturerController
+    {
+    }
+}
---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Management_API.Models;
+
+namespace Student_Management_API.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options ) : base(options)
+        {
+            
+        }
+        public DbSet <Courses> Courses { get; set; }
+        public DbSet <Department> Departments { get; set; }
+        public DbSet <Enrollment> Enrollments { get; set; }
+        public DbSet <Lecturers> Lecturers{ get; set; }
+        public DbSet <Students> Students { get; set; }
+    }
+}
